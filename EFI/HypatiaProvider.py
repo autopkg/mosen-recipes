d4bae97b7e1507edcbfa23f99d8134223755d0bb
Elliot Jordan <elliot@elliotjordan.com>
2019-08-31 21:54:27 -0700

    Remove unused imports

--- EFI/HypatiaProvider.py
+++ EFI/HypatiaProvider.py
@@ -14,10 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
-from __future__ import print_function
-from xml.etree import ElementTree
-import urllib
+from __future__ import absolute_import, print_function
+
 import urllib2
 
 from autopkglib import Processor, ProcessorError
